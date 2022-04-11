ba6c361340b9a50818b8d0c59c22288c14edce22
Chris <kimballc3@yahoo.com>
2022-04-10 23:17:37 -0500

    starting to get html setup

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const apiKey = "7cb074548a966565cd86511a34008fcf"
+
+// this is the url to fetch the data from
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
